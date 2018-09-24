9f070b4a3c7187e1dffafa1a538d697daf2f5a6f
Chris Lu <chris.lu@gmail.com>
2018-09-24 12:57:57 -0700

    trying to release with release.sh
    Reviewers: zichuan
    
    Reviewed By: zichuan
    
    Differential Revision: https://code.uberinternal.com/D2099173

--- .jenkins/release.sh
+++ .jenkins/release.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env bash
 
+release_opts="-Prelease.useAutomaticVersion=true"
+if [ -n "${RELEASE_VERSION}" ]; then
+        release_opts="${release_opts} -Prelease.releaseVersion=${RELEASE_VERSION}"
+fi
+if [ -n "${NEXT_VERSION}" ]; then
+        release_opts="${release_opts} -Prelease.newVersion=${NEXT_VERSION}"
+fi
+
 ./gradlew clean :core:release --stacktrace ${release_opts} -x updateVersion -x commitNewVersion
